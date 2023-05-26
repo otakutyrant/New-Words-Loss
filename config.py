e59610e5647099400482972d00c7d931845aa998
otakutyrant <otakutyrant@gmail.com>
2023-05-26 15:19:27 +0800

    config.py: arrange the layout

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 from calibre.utils.config import JSONConfig
-from qt.core import QFileDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from qt.core import QFileDialog, QGridLayout, QLabel, QLineEdit, QPushButton, QWidget
 
 PLUGIN_ICONS = ["images/new_words.png"]
 
@@ -22,26 +22,26 @@ prefs.defaults["learned_words_pathname"] = Path.home() / "learned_words.txt"
 class ConfigWidget(QWidget):
     def __init__(self):
         super().__init__()
+        self.setGeometry(20, 20, 20, 20)
 
-        self.label = QLabel("Learned Words txt path:")
+        layout = QGridLayout()
+        self.setLayout(layout)
 
-        self.learned_words_pathname_line_edit = QLineEdit()
+        label = QLabel("Learned Words txt path:")
+        layout.addWidget(label, 0, 0, 1, 1)
+
+        self.learned_words_pathname_line_edit = QLineEdit(self)
         self.learned_words_pathname_line_edit.setPlaceholderText(
             "Choose the path of the learned words file"
         )
         self.learned_words_pathname_line_edit.setText(
             str(prefs["learned_words_pathname"])
         )
+        layout.addWidget(self.learned_words_pathname_line_edit, 0, 1, 1, 2)
 
-        self.choose_path_button = QPushButton("Choose Path")
-        self.choose_path_button.clicked.connect(self.set_learned_words_pathname)
-
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.learned_words_pathname_line_edit)
-        layout.addWidget(self.choose_path_button)
-
-        self.setLayout(layout)
+        choose_path_button = QPushButton("Choose Path")
+        choose_path_button.clicked.connect(self.set_learned_words_pathname)
+        layout.addWidget(choose_path_button, 0, 2, 1, 1)
 
     def set_learned_words_pathname(self):
         path, _ = QFileDialog.getOpenFileName(
